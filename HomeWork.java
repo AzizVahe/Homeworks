10473f27bdf5a0cec27bb98fc88c15f2d3ddbf9a
AzizVahe <v888e@mail.ru>
2021-11-27 21:48:40 +0400

    My First Homework

---
+++ HomeWork.java
@@ -0,0 +1,11 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int num = 897;
+        for( ; num >= 798; num--){
+            if(num > 798){
+                continue;
+            }
+            System.out.println(num);
+        }
+    }
+}
