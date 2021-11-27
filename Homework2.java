82aba3309f95967638182a102346ad91b3ee2cca
AzizVahe <v888e@mail.ru>
2021-11-27 23:17:38 +0400

    the second variant

---
+++ Homework2.java
@@ -0,0 +1,9 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int num = 897;
+        do {
+            num--;
+        }while (num >798 && num != 798);
+            System.out.println(num);
+    }
+}
